0eb721a60f9bfbc7e7c1b4a34ab7cc064a836292
Bonilla <jarodriguezbonilla@gmail.com>
2026-06-16 13:39:32 +0200

    Remove temporary db fix script

--- update_trades.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-\App\Models\Trade::whereNull('bot_execution_id')->update(['is_dry_run' => true]);
-echo "Updated manual trades successfully\n";

fe34739c9f74b3ac5d4efefd4d94f75aedd61af8
Bonilla <jarodriguezbonilla@gmail.com>
2026-06-16 16:56:47 +0200

    Rename G/P no realizado to Ganancia / Pérdida in portfolio view

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -860,7 +860,7 @@
                                 </th>
                                 <th class="py-4 px-5 text-right">
                                     <div class="flex items-center justify-end gap-1.5">
-                                        <span class="text-slate-500">G/P no realizado</span>
+                                        <span class="text-slate-500">Ganancia / Pérdida</span>
                                         <div class="relative inline-block" x-data="{ open: false }" @mouseenter="open = true" @mouseleave="open = false" @click.away="open = false">
                                             <svg @click="open = !open" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" class="w-3.5 h-3.5 text-slate-400 hover:text-indigo-400 transition-colors cursor-pointer">
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />

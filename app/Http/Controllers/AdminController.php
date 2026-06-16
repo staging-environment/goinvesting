b878f56e6493dbd6a51ad45ebc4bb6e21a52dedc
Bonilla <jarodriguezbonilla@gmail.com>
2026-06-16 09:45:46 +0200

    fix: remove controller-level middleware from AdminController

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,11 +7,6 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Show admin dashboard.
      */

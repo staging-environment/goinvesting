07ef8425d3455fa20ee8360076bb9ee942439a14
Bonilla <jarodriguezbonilla@gmail.com>
2026-07-13 18:21:45 +0200

    temp: add autoload requirement to delete_migration helper

--- delete_migration.php
+++ delete_migration.php
@@ -1,4 +1,5 @@
 <?php
+require __DIR__.'/vendor/autoload.php';
 require __DIR__.'/bootstrap/app.php';
 $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 \Illuminate\Support\Facades\DB::table('migrations')->where('migration', '2026_07_13_161733_add_trailing_and_dca_fields_to_trades')->delete();
